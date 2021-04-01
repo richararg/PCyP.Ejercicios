8d4bf643e74409f8dc40dddbeb3b244b662734a7
richararg <ricardogonzalez78@gmail.com>
2021-04-01 15:11:14 -0300

    Commit - Ejercicio 1 - Vial

---
+++ Ejercicio1.Vial/Objeto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Vial
+{
+    class Objeto
+    {
+        int numeroDeHoja;
+        string tipoObjeto;
+        long idTramo;
+        string tipoCamino;
+        int longitud;
+
+        public Objeto()
+        {
+            numeroDeHoja = 0;
+            tipoObjeto = null;
+            idTramo = 0;
+            tipoCamino = null;
+            longitud = 0;
+        }
+
+        public Objeto(int nro, string tipoO, long id, string tipoC, int long_)
+        {
+            numeroDeHoja = nro;
+            tipoObjeto = tipoO;
+            idTramo = id;
+            tipoCamino = tipoC;
+            longitud = long_;
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("Número de hoja: {0}\tTipo de objeto: {1}\tIdentificador de tramo: {2}\tTipo de camino: {3}\tLongitud: {4}", numeroDeHoja, tipoObjeto, idTramo, tipoCamino, longitud);
+        }
+    }
+}
---
+++ Ejercicio1.Vial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Vial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Guardo el archivo en un variable array string
+                string[] lineas = File.ReadAllLines(@"vial.csv");
+
+                // instancio Objeto
+                Objeto ob;
+                int cont = 0;
+
+                // por cada linea de mi array 
+                foreach (var linea in lineas)
+                {
+                    // utilizo el separador , en la cadena de caracteres
+                    var valores = linea.Split(',');
+
+                    ob = new Objeto(int.Parse(valores[0]), valores[1], long.Parse(valores[2]), valores[3], int.Parse(valores[4]));
+                    ob.imprimir();
+                    cont++;
+                }
+
+                Console.WriteLine("Total de líneas leídas: " + cont);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
