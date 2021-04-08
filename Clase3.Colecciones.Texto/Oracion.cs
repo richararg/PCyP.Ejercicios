b9c2612edadf83c606b52f84de383310e7f32752
richararg <ricardogonzalez78@gmail.com>
2021-04-08 19:21:44 -0300

    Resuelto ejercicio 3
    Faltaba resolucion del ejercicio

---
+++ Clase3.Colecciones.Texto/Oracion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Colecciones.Texto
+{
+    class Oracion
+    {
+        private Dictionary<string, int> parrafo = new Dictionary<string, int>();
+
+        // Metodos
+        
+        // Constructor con texto
+        public Oracion (string t)
+        {
+            // Separo el texto por espacio
+            var texto = t.Split(' ');
+            // Por cada string lo almaceno en x
+            foreach(string x in texto)
+            {
+                // Verificando si existe x en el diccionario
+                if (parrafo.ContainsKey(x))
+                {
+                    // Si existe agrego 1 a valor
+                    parrafo[x] = parrafo[x] + 1;
+                } else
+                {
+                    // Si no existe lo agrego con valor 1
+                    parrafo.Add(x, 1);
+                }
+            }
+        }
+
+
+        public void resultado()
+        {
+
+            // Muestro por consola el analisis
+            Console.WriteLine("\nContenido del diccionario:\nKey:\t\tValue:");
+            foreach(KeyValuePair<string, int> x in parrafo)
+            {
+                Console.WriteLine(x.Key + "\t\t" + x.Value);
+            }
+            Console.WriteLine("Size: {0}", parrafo.Count);
+
+        }
+
+
+    }
+}
---
+++ Clase3.Colecciones.Texto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Colecciones.Texto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Realizar un software que solicite al usuario que ingrese un párrafo 
+            // por teclado y el software cuente la cantidad de ocurrencias de cada 
+            // palabra. Asimismo, deberá indicar la cantidad de palabras distintas 
+            // que existen en el párrafo ingresado por el usuario.
+
+            Console.WriteLine("Ingrese el texto a analizar: ");
+            string texto = Console.ReadLine();
+            Oracion o = new Oracion(texto);
+            o.resultado();
+
+        }
+    }
+}
