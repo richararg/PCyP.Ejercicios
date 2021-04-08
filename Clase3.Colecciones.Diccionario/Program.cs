5fac49bb662c78e51eb8842654284fd15e500e5b
richararg <ricardogonzalez78@gmail.com>
2021-04-08 15:11:03 -0300

    Clase3.Ejercicios
    Resueltos ejercicios 1,2,3

---
+++ Clase3.Colecciones.Diccionario/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Colecciones.Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implementar un diccionario que contendrá los siguientes valores:
+            // values.Add("Juan", "55423412");
+            // values.Add("Ernesto", "56985623");
+            // values.Add("Mariana", "54787451");
+            // Realice las siguientes operaciones:
+            // a.Revisar en el diccionario si existe un índice llamado “Juan”. En caso de que exista,
+            // mostrar su valor(utilice el método ContainsKey)
+            // b.Revisar en el diccionario si existe un índice llamado “Pedro”. En caso de que exista,
+            // mostrar su valor, si no existe imprimir por pantalla “No contiene la llave” (utilice el método TryGetValue)
+            // c.Recorrer el diccionario y mostrar todos sus datos (índice y valor).
+            // d.Alterar el valor cuyo índice es “Mariana” por “58251425”. Imprimir el nuevo valor.
+
+
+            // Inicializo Dictionary nombrado values
+            Dictionary<String, String> values = new Dictionary<string, string>();
+
+            // Agrego Valores
+            values.Add("Juan", "55423412");
+            values.Add("Ernesto", "56985623");
+            values.Add("Mariana", "54787451");
+
+            // a.
+            Console.WriteLine("Ejercicio a.\n");
+            String buscado = "Juan";
+
+            if (values.ContainsKey(buscado))
+            {
+                foreach (KeyValuePair<string, string> x in values)
+                {
+                    if (x.Key.Equals(buscado))
+                    {
+                        Console.WriteLine("El valor de " + buscado + " es de: " + x.Value);
+                    }
+                }
+            }
+
+
+            // b.
+            Console.WriteLine("\nEjercicio b.\n");
+            buscado = "Pedro";
+            String valor;
+            if (values.TryGetValue(buscado, out valor))
+            {
+                        Console.WriteLine("El valor de " + buscado + " es de: " + valor);
+            } else
+            {
+                Console.WriteLine("No contiene llave");
+            }
+
+            //c.
+            Console.WriteLine("\nEjercicio c.\n");
+            foreach (KeyValuePair<string,string> z in values)
+            {
+                Console.WriteLine("Indice: {0} \t Valor: {1}", z.Key, z.Value);
+            }
+
+            //d.
+            Console.WriteLine("\nEjercicio d.\n");
+            Console.WriteLine("El valor de Mariana es de: {0}", values["Mariana"]);
+            values["Mariana"] = "58251425";
+            Console.WriteLine("El nuevo valor de Mariana es de: {0}", values["Mariana"]);
+
+        }
+    }
+}
---
+++ Clase3.Colecciones.Listas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Colecciones.Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Dado dos vectores:
+            // private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+            // private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+            // Realice las siguientes operaciones:
+            // a.Armar dos listas, una con los valores del vector “colors” y otra con los valores del vector “removeColors”.
+            // b.Mostrar la lista de colores por pantalla.
+            // c.De la lista de colores eliminar los colores listados en la otra lista(removeColors).
+            // d.Volver a mostrar la lista de colores por pantalla.
+
+            // a.
+            List<string> colors = new List<string> { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+            List<string> removecolors = new List<string> { "RED", "WHITE", "BLUE" };
+            
+             // b.
+            Console.WriteLine("\nEjercicio b.\n");
+            
+            foreach(string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            // c.
+   
+            foreach (string borrar in removecolors)
+            {
+                colors.Remove(borrar);
+            }
+
+            // d.
+
+            Console.WriteLine("\nEjercicio d.\n");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+        }
+    }
+}
